474dc6c9493c0eb46466fe6dc8d0621a4b64031d
cmanzanof <62973531+cmanzanof@users.noreply.github.com>
2025-04-20 07:35:22 -0400

    Update main.py
    permitiendo al frontend hacer contacto

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# Configura CORS para permitir el frontend de Vercel
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://front-iglesia.vercel.app"],  # Cambia esto por tu dominio de Vercel
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"mensaje": "¡Hola desde FastAPI en Railway!"}
